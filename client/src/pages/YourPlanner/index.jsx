9efd35f4796ffc5dee6a0741e5e5bd85053df883
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-08-04 16:07:20 -0400

    add folders

--- client/src/pages/YourPlanner/index.jsx
+++ client/src/pages/YourPlanner/index.jsx
@@ -7,28 +7,10 @@ import Auth from '../../utils/auth';
 
 export default function ViewPlanner() {
     const navigate = useNavigate();
-    // const [timeState, setTimePost] = useState(
-    //     [{
-    //         time : '9am', 
-    //         activity : ''},
-    //         {time : '12pm',
-    //         activity : ''},
-    //         {time : '3pm',
-    //         activity : ''},
-    //         {time : '7pm',
-    //         activity : ''},
-    //         {time : '11pm',
-    //         activity : ''},
-    //     ] 
-    // );
+
     const [planState, setPlanState] = useState({});
     const times = ['9am', '12pm', '3pm', '6pm'];
 
-    // useEffect(()=>{
-
-    //     // here grab all of the data from the backend and set it to state
-    //     // setUserPost({posts: data from backend})
-    // }, [planState])
 
     const { loading, data } = useQuery(QUERY_PLAN_BY_USER, {
         // variables: { username: Auth.getProfile().data.username }
@@ -50,11 +32,7 @@ export default function ViewPlanner() {
     console.log(planState[0]);
 
     const handleFilter = (event) => {
-        // check out event.target.value and .filter method.
-        // youre going to need to filter and set event.target.value to filteredPosts
-        // filteredPosts is what youre going to want to display and map over down below
         console.log(event.target.value)
-
     }
 
     function viewPlan(plan){
@@ -81,16 +59,7 @@ export default function ViewPlanner() {
             description: plan.descriptionText,
             days: daysData
         });
-
-        // setUserPost(
-        //     [{
-        //         time : '10am', 
-        //         activity : ''
-        //     },]
-        // )
     }
-
-    // const days = planState[1]
     console.log(planState.days && planState.days[0]);
 
     return (
@@ -180,30 +149,6 @@ export default function ViewPlanner() {
 
             <br></br>
 
-            {/* <div className="row">
-                <div className="col-12">
-                    <div className='grandparent'>
-
-                        <div className='plan-title'>Plan Created by:  user123</div>
-
-                        <div className='plan-likes'>
-                            <div className="vote roundrect">
-                                <div className="increment up"></div>
-                                <div className="increment down"></div>
-                                <button className="likes">This Travel Plan has been liked by  : 105 Travel Addicts</button>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </div> */}
         </section>
     );
 }
-
-// when user comes to page, they see list of plans, when they click the it populates - this will fetch plan
-// when edit button is clicked, you go to create plan 
-//  make component for pop up 
-// activity has unuqie id, the relationahip is that the base of the plan has the days array, which is day model which has day activity , activity must know whuch activity and at 9am the days 
-// is the master relationship
-// implement the back end to make the buttons work 
