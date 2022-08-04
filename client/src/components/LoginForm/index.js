609f30508499f97f72ec5205231ac571469d4889
Yurippe <yurichikawa1992@gmail.com>
2022-08-04 19:39:11 -0400

    changed overall styles of yourPlanner page

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { Navigate } from "react-router-dom";
-import { LOGIN_USER } from "../utils/mutations";
 import { useMutation } from "@apollo/client";
-import Auth from "../utils/auth";
+import { LOGIN_USER } from "../../utils/mutations";
+import Auth from "../../utils/auth";
 
 export default function LoginForm() {
     const [loginUser, { error }] = useMutation(LOGIN_USER);
--- client/src/pages/YourPlanner/index.jsx
+++ client/src/pages/YourPlanner/index.jsx
@@ -1,38 +1,18 @@
-import React, { useEffect, useState }from "react";
-import "./ViewPlanner.css";
-import {useNavigate} from "react-router-dom"
-import { QUERY_PLAN_BY_USER } from '../../utils/queries';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { useQuery } from '@apollo/client';
+
+import { QUERY_PLAN_BY_USER } from '../../utils/queries';
 import Auth from '../../utils/auth';
+import "./index.css";
 
 export default function ViewPlanner() {
     const navigate = useNavigate();
-    // const [timeState, setTimePost] = useState(
-    //     [{
-    //         time : '9am', 
-    //         activity : ''},
-    //         {time : '12pm',
-    //         activity : ''},
-    //         {time : '3pm',
-    //         activity : ''},
-    //         {time : '7pm',
-    //         activity : ''},
-    //         {time : '11pm',
-    //         activity : ''},
-    //     ] 
-    // );
     const [planState, setPlanState] = useState({});
     const times = ['9am', '12pm', '3pm', '6pm'];
 
-    // useEffect(()=>{
-
-    //     // here grab all of the data from the backend and set it to state
-    //     // setUserPost({posts: data from backend})
-    // }, [planState])
-
     const { loading, data } = useQuery(QUERY_PLAN_BY_USER, {
         // variables: { username: Auth.getProfile().data.username }
-        //  variables: { username: 'Taylor.Jaskolski' }
         variables: { username: 'Steve.Ruecker52' }
     });
 
@@ -46,19 +26,7 @@ export default function ViewPlanner() {
 
     const plans = data?.searchPlansByUser.myPlans;
 
-    console.log(plans);
-    console.log(planState[0]);
-
-    const handleFilter = (event) => {
-        // check out event.target.value and .filter method.
-        // youre going to need to filter and set event.target.value to filteredPosts
-        // filteredPosts is what youre going to want to display and map over down below
-        console.log(event.target.value)
-
-    }
-
     function viewPlan(plan){
-        console.log(plan);
         const daysData = plan.days.map(day => {
             const activities = day.activities.map(activity => {
                 return {
@@ -78,107 +46,96 @@ export default function ViewPlanner() {
         setPlanState({
             id: plan._id,
             title: plan.planTitle,
+            destination: plan.destination,
             description: plan.descriptionText,
             days: daysData
         });
-
-        // setUserPost(
-        //     [{
-        //         time : '10am', 
-        //         activity : ''
-        //     },]
-        // )
     }
 
-    // const days = planState[1]
-    console.log(planState.days && planState.days[0]);
-
     return (
-        <section className="viewplan row px-5" >
+        <section className="row px-5" >
             <article className="col-12 col-md-2 col-xl-3">
-                <div className="">
-
-                    <div className='parent'>
-                        <h2 className="plan-title">My Plans</h2>
-
-                        <button className="plan-nickname" onClick={function(){
-                            navigate("/Planner")
-                        }} >Edit Plan</button>
-                    </div>
-
-                    <div className="vstack gap-3">
-                        <br></br>
-                        {plans.length && plans.map(plan => {
-                            return (
-                                <button 
-                                    key={plan._id} 
-                                    onClick={() => {viewPlan(plan)}} 
-                                    className="user-plans"
-                                >
-                                    {plan.planTitle}
-                                </button>
-                            );
-                        })}
-                    </div>
+                <h2>My Plans</h2>
+                <div className="vstack gap-3 mt-4">
+                    {plans.length && plans.map(plan => {
+                        return (
+                            <button 
+                                key={plan._id} 
+                                onClick={() => {viewPlan(plan)}} 
+                                className="user-plans"
+                            >
+                                {plan.planTitle}
+                            </button>
+                        );
+                    })}
                 </div>
-                
             </article>
-        
 
             <article className="col-6 col-md-10 col-xl-9">
-
-                <div className="row">
+                <div className="row px-3">
                     <div className='col-12 d-flex justify-content-center'>
                         <h2 className="plan-details">Plan Details</h2>
+                        {planState.id && (
+                            <div className='my-4'>
+                                <button 
+                                    className="edit-btn" 
+                                    onClick={function(){navigate("/Planner")}}
+                                >
+                                    Edit Plan
+                                </button>
+                            </div>
+                        )}
                     </div>
-                
-                    <div className='col-12 mx-5 px-3 pt-4'>
+                    
+                    {planState.id ? (
+                        <div className='col-12 mx-4'>
+                            <div className="col-6 col-md-3 col-xl-3 row">
+                                <h3>Plan Title</h3>
+                                <p>{planState.title}</p>
 
-                        <div className="col-6 col-md-3 col-xl-3 row">
-                            <h3 className='details-subtitle col-12'>Description</h3>
-                            <div className='description border border-secondary col-11'>
-                                <p>
-                                    {planState.description ? 
-                                        planState.description 
-                                        : 
-                                        'Please select a title to see details.'
-                                    }
+                                <h3 className='mt-3'>Destination</h3>
+                                <p>{planState.destination}</p>
+
+                                <h3 className='mt-3'>Description</h3>
+                                <p className='description border border-secondary p-3 col-11'>
+                                    {planState.description}
                                 </p>
                             </div>
-                        </div>
-                        <div className="col-8 col-md-9 col-xl-8 row">
-                            <h3 className='details-subtitle col-12'>Time Schedule</h3>
-                            <div className="col-12 d-flex">
 
-                                {planState.days && planState.days.map(day => {
-                                    return (
-                                        <div key={day.id} className="mx-4 w-50">
-                                            <h4>{day.dayNumber}</h4>
-                                            {times.map(time => {
-                                                return (
-                                                    <div key={time} className='row'>
-                                                        <p className='col-3'>{time}</p>
-                                                        {day.activities && day.activities.map(activity => {
-                                                            if (activity.time === time) {
-                                                                return (
-                                                                    <p key={activity.id} className='col-7'>{activity.name}</p>
-                                                                );
-                                                            }
-                                                        })}
-                                                    </div> 
-                                                );
-                                            })}
+                            <div className="col-8 col-md-9 col-xl-8 row">
+                                <h3 className='details-subtitle col-12'>Time Schedule</h3>
+                                <div className="col-12 d-flex">
 
-                                        </div>
-                                    )
-                                })}
+                                    {planState.days && planState.days.map(day => {
+                                        return (
+                                            <div key={day.id} className="mx-4 w-50">
+                                                <h4>{day.dayNumber}</h4>
+                                                {times.map(time => {
+                                                    return (
+                                                        <div key={time} className='row'>
+                                                            <p className='col-3'>{time}</p>
+                                                            {day.activities && day.activities.map(activity => {
+                                                                if (activity.time === time) {
+                                                                    return (
+                                                                        <p key={activity.id} className='col-7'>{activity.name}</p>
+                                                                    );
+                                                                }
+                                                            })}
+                                                        </div> 
+                                                    );
+                                                })}
+
+                                            </div>
+                                        )
+                                    })}
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    ) : (
+                        <p className='mt-4'>Please select a title to see details.</p>
+                    )}
                 </div>
             </article>
-
-            <br></br>
 
             {/* <div className="row">
                 <div className="col-12">
@@ -200,10 +157,3 @@ export default function ViewPlanner() {
         </section>
     );
 }
-
-// when user comes to page, they see list of plans, when they click the it populates - this will fetch plan
-// when edit button is clicked, you go to create plan 
-//  make component for pop up 
-// activity has unuqie id, the relationahip is that the base of the plan has the days array, which is day model which has day activity , activity must know whuch activity and at 9am the days 
-// is the master relationship
-// implement the back end to make the buttons work 
