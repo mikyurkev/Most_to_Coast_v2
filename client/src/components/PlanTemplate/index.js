a1c5b8b401fd7ddcfd00072a043926b97df8f6f4
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-08-04 23:43:38 -0400

    add query to populate the added days

--- client/src/components/PlanTemplate/index.js
+++ client/src/components/PlanTemplate/index.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
-import { useQuery, useMutation } from "@apollo/client";
+import { useQuery, useLazyQuery, useMutation } from "@apollo/client";
 import {CREATE_DAY} from "../../utils/mutations";
+import {QUERY_PLAN_BY_ID} from "../../utils/queries";
 import {CREATE_ACTIVITY} from "../../utils/mutations";
 
 export default function PlanTemplate(props) {
     const [addDay] = useMutation(CREATE_DAY);
     const [addActivity] = useMutation(CREATE_DAY);
-
+    const [getPlan, { data: planData }] = useLazyQuery(QUERY_PLAN_BY_ID);
     const [day, setDay] = useState(1);
     // console.log(props.planId.createPlan._id);
     // let planId = props.planId.createPlan._id;
@@ -23,16 +24,25 @@ export default function PlanTemplate(props) {
               variables: { planId: props.planId.createPlan._id, input: {dayNumber: day} }
             });
 
-            
+
           } catch (e) {
             console.error(e);
         }
+
+        console.log(props.planId.createPlan._id);
+
     };
+
+    const handleFetch = async(event) => {
+        event.preventDefault();
+        getPlan({ variables: { id: props.planId.createPlan._id } })
+        console.log(planData);
+    }
 
     const handleFormSubmit = async(event) => {
         try {
             await addActivity({
-              variables: { input: {  } }
+              variables: { input: { } }
             });
 
           } catch (e) {
@@ -45,11 +55,21 @@ export default function PlanTemplate(props) {
     return (
         <div className="container">
             <button onClick={handleAddDay}>Add Day</button>
-            <h1>Day 1</h1>
-            <p>9am</p>
-            <p>12am</p>
-            <p>3pm</p>
-            <p>6pm</p>
+            <button onClick={handleFetch}>Check Day</button>
+            <div>
+            {planData ? planData.singlePlan.days.map((days) => {
+                return (
+                    <div className="text-left">
+                        <h2>{days.dayNumber}</h2>
+                        <p>9am</p>
+                        <p>12am</p>
+                        <p>3pm</p>
+                        <p>6pm</p>
+                    </div>
+                )
+            }) : ('No Data yet')
+            }
+            </div>
 
             <h1>{day}</h1>
         </div>
