7e53dc36412b232f4c4d3f8c3e6e699a050fa4c5
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-08-06 00:37:46 -0400

    add activities state but debug is needed
    need to create another component to better store the form, also use object in state, then it only need one function to manage the form

--- client/src/components/PlanTemplate/index.js
+++ client/src/components/PlanTemplate/index.js
@@ -8,11 +8,9 @@ export default function PlanTemplate(props) {
     const [addDay] = useMutation(CREATE_DAY);
     const [addActivity] = useMutation(CREATE_DAY);
     const [getPlan, { data: planData }] = useLazyQuery(QUERY_PLAN_BY_ID);
-    const [day, setDay] = useState(1);
-    // console.log(props.planId.createPlan._id);
-    // let planId = props.planId.createPlan._id;
-    // console.log(planId);
-    
+    const [day, setDay] = useState(0);
+
+    //Add Day Portion
     const handleAddDay = async(event) => {
         
         setDay(day + 1);
@@ -39,6 +37,37 @@ export default function PlanTemplate(props) {
         console.log(planData);
     }
 
+    if(planData){
+        console.log(planData.singlePlan.days.length);
+        for (var i = 0; i< planData.singlePlan.days.length; i++) {
+            
+        }
+    }
+
+
+
+    //Add Activity Portion
+    //text from 9am
+    const [dayInfo1, setdayInfo1] = useState();
+    const handleChange1 = (event) => {
+        setdayInfo1(event.target.value);
+    };
+    //text from 1pm
+    const [dayInfo2, setdayInfo2] = useState();
+    const handleChange2 = (event) => {
+        setdayInfo2(event.target.value);
+    };
+    //text from 3pm
+    const [dayInfo3, setdayInfo3] = useState();
+    const handleChange3 = (event) => {
+        setdayInfo3(event.target.value);
+    };
+    //text from 5pm
+    const [dayInfo4, setdayInfo4] = useState();
+    const handleChange4 = (event) => {
+        setdayInfo4(event.target.value);
+    };
+
     const handleFormSubmit = async(event) => {
         try {
             await addActivity({
@@ -60,18 +89,28 @@ export default function PlanTemplate(props) {
             {planData ? planData.singlePlan.days.map((days) => {
                 return (
                     <div className="text-left">
-                        <h2>{days.dayNumber}</h2>
-                        <p>9am</p>
-                        <p>12am</p>
-                        <p>3pm</p>
-                        <p>6pm</p>
+                        <h2>Day {days.dayNumber + 1} </h2>
+                        <form>
+                            <label>9am: </label>
+                            <input type="text" onChange={handleChange1} value={dayInfo1}></input>
+                            <br></br>
+                            <label>12am: </label>
+                            <input type="text" onChange={handleChange2} value={dayInfo2}></input>
+                            <br></br>
+                            <label>3pm: </label>
+                            <input type="text" onChange={handleChange3} value={dayInfo3}></input>
+                            <br></br>
+                            <label>6pm: </label>
+                            <input type="text" onChange={handleChange4} value={dayInfo4}></input>
+                        </form>
                     </div>
                 )
             }) : ('No Data yet')
             }
             </div>
-
-            <h1>{day}</h1>
+            <button>Rock and Roll!</button>
+            <h1>Number of days: {day}</h1>
+            
         </div>
     )
 };
