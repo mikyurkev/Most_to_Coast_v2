1216191a993fce30a6213bbba054d904957a843a
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-08-09 00:36:59 -0400

    add slogan

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -15,8 +15,9 @@ const Header = () => {
 
   return (
     <header>
-
+      <h1 className="justify-content-center">Most to Coast - Your next trip planning expert!</h1>
       <div className="navbar">
+      
         <ul>
           <li>
             <NavLink to="/main"><img src="img/Most2Coast.png" className="logo-header" alt="airplanelogo" /></NavLink>
