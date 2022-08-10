588b486cf078e634061910791a4d7d02ac9a6d42
Yurippe <yurichikawa1992@gmail.com>
2022-08-10 19:49:21 -0400

    change db name in connection file

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Most_to_Coast',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/most-to-coast-v2',
   {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
